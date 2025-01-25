558b877ed5c6174d1b4814c4195f736e5ea1e852
Saman Ashoori <saman@samanashoori.com>
2025-01-25 21:56:20 +0000

    Create insert-transaction.sql

---
+++ insert-transaction.sql
@@ -0,0 +1,5 @@
+INSERT INTO Transaction (Drinkid, Storeid, Transactioncost, Transactiondate)
+VALUES (1, 1, 4.5, '2025-01-16');
+
+INSERT INTO Transaction (Drinkid, Storeid, Transactioncost, Transactiondate)
+VALUES (2, 1, 3.5, '2025-01-16');
